42496eb90545f17f2c133de723b11ca3af970dc8
Radmir Musabaev <musabaevradmir@gmail.com>
2023-05-08 22:29:59 +0400

    small changes

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -1,11 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {useAppSelector} from "../hooks/useTyped";
 import CartItem from "./CartItem";
+import {IProduct} from "../types";
 
-const CartList:FC = () => {
+const CartList: FC = () => {
     const {cartItems} = useAppSelector(state => state.cart)
+    const {products} = useAppSelector(state => state.product)
+    const [cost, setCost] = useState<number>(0)
 
-
+    useEffect(() => {
+        setCost(0)
+        let tempCost: number = 0;
+        let product;
+        cartItems.map(item => {
+                product = products.find(i => i.id === item.productId)
+                if (product) {
+                   tempCost += product.price;
+                }
+            }
+        )
+        console.log(tempCost)
+        setCost(tempCost)
+    }, [cartItems])
 
     return (
         <div>
@@ -13,9 +29,9 @@ const CartList:FC = () => {
                 <div>{cartItems.map(cartItem =>
                     <CartItem productId={cartItem.productId} count={cartItem.count}/>
                 )}
-                <div>
-                    Purchase amount is:
-                </div>
+                    <h2>
+                        Purchase amount is: {cost} $
+                    </h2>
                 </div>
                 : <div>No items in the Cart</div>
             }
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -16,21 +16,27 @@ const ProductItem: FC<IProductItem> = (productItem) => {
 
     return (
         <div className={classes.body}>
-            <div className={classes.image}>
-                <img
-                    src={productItem.productItem.image}
-                    height='200px'
-                    width='200px'
-                    style={{objectFit: "contain", background: "white"}}
-                />
+            <div className={classes.mainInfo}>
+                <div className={classes.image}>
+                    <img
+                        src={productItem.productItem.image}
+                        height='200px'
+                        width='200px'
+                        style={{objectFit: "contain", background: "white"}}
+                    />
+                </div>
+                <h2 style={{background: "white"}}>{productItem.productItem.title}</h2>
+                <div className={classes.category}>
+                    Category: {productItem.productItem.category}
+                </div>
+                <div className={classes.description}>
+                    {productItem.productItem.description}
+                </div>
+                <div className={classes.price}>
+                    Price: {productItem.productItem.price} $
+                </div>
             </div>
-            <h2 style={{background: "white"}}>{productItem.productItem.title}</h2>
-            <div className={classes.category}>
-                Category: {productItem.productItem.category}
-            </div>
-            <div className={classes.description}>
-                {productItem.productItem.description}
-            </div>
+
             <div className={classes.button}>
                 <Button onClick={() => dispatch(addToCart(toCartItem(productItem.productItem.id, 1)))}>Add to card</Button>
             </div>
